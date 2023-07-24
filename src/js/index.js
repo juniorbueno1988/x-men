25227ed7ab7f3c34e8bbd171d530acec2145ec24
Júnior Bueno <125302780+juniorbueno1988@users.noreply.github.com>
2023-07-24 17:54:04 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+//passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
+
+const personagens = document.querySelectorAll('.personagem');
+
+//passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450{
+            window.scrollTo({top: 0, behavior: 'smooh'});
+        })
+
+        //passo 3 - verificar se já existe um personagem selecionado, se sim, devemos removar a seleção dele
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        // OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o ome e a descriição do personagem grande.
+
+        //passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        //passo 2 - alterar a imagem do personagem grande
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+
+        // passo 3 - alterar o nome do personagem grae
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        // passo 4 - alterar a descrição do personagem grande
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+        
+    })
+})
